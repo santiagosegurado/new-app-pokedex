24d7c760f3d2f29499ee39030937c3e2a8e38b70
Santiago Segurado <santisegurado1@gmail.com>
2022-08-11 16:09:45 -0300

    Docs Actualizado

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import App from './App';
 import './index.css'
 import { PokemonScreen } from './components/PokemonScreen';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.querySelector('#root'));
 console.log('Hola');
 root.render(
     <BrowserRouter>
